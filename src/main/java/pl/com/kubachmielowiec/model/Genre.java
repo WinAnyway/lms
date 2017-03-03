d3af3cbe4158dc95546714cbaf785cf56c6704fe
WinAnyway <kuba.chmielowiec@gmail.com>
2017-03-03 13:53:07 +0100

    Genre equals and Hashcode added

--- src/main/java/pl/com/kubachmielowiec/model/Genre.java
+++ src/main/java/pl/com/kubachmielowiec/model/Genre.java
@@ -16,4 +16,20 @@ public class Genre {
     public Genre(String name) {
         this.name = name;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Genre genre = (Genre) o;
+
+        return name.equals(genre.name);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
 }
