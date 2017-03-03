6d9129dad68f63969825b9eddf1c22f4466a2609
WinAnyway <kuba.chmielowiec@gmail.com>
2017-03-03 14:56:01 +0100

    Loan class mapped

--- src/main/java/pl/com/kubachmielowiec/LmsApplication.java
+++ src/main/java/pl/com/kubachmielowiec/LmsApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-//    D:\hsqldb-2.3.4\hsqldb>java -cp lib/hsqldb.jar org.hsqldb.server.Server --database.0 file:D:/lmsResources/lmsDB --dbname.0 lms
+//    D:\hsqldb-2.3.4\hsqldb
+//    java -cp lib/hsqldb.jar org.hsqldb.server.Server --database.0 file:D:/lmsResources/lmsDB --dbname.0 lms
 
 @SpringBootApplication
 public class LmsApplication {
--- src/main/java/pl/com/kubachmielowiec/model/Loan.java
+++ src/main/java/pl/com/kubachmielowiec/model/Loan.java
@@ -1,7 +1,19 @@
 package pl.com.kubachmielowiec.model;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
 public class Loan {
 
+    @Id
+    @GeneratedValue
+    private Long id;
 
+    @OneToOne
+    private Publication publication;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date loanDate;
 
 }
